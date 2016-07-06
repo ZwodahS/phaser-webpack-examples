b76f5f19551744265d72187037c4ff0c3b0fcfe1
Eric Ng <ericnjf@gmail.com>
2016-07-06 07:24:45 -0400

    feat(game) add invaders webpack

---
+++ invaders/README.md
@@ -0,0 +1,10 @@
+# Setup
+
+```
+npm install
+npm run build
+```
+
+You will see invaders.bundle.js inside the dist folder after that.
+Link up the assets from the original repo and put this dist (or symlink) it to your static folder to play.
+
---
+++ invaders/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+
+// Phaser webpack config
+let phaserModule = path.join(__dirname, '/node_modules/phaser/');
+let phaser = path.join(phaserModule, 'build/custom/phaser-split.js');
+let pixi = path.join(phaserModule, 'build/custom/pixi.js');
+let p2 = path.join(phaserModule, 'build/custom/p2.js');
+
+let loaders = [
+    {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel',
+        query: {
+            presets: ['es2015']
+        }
+    },
+    { test: /phaser-split\.js$/, loader: 'expose?Phaser' },
+    { test: /pixi\.js/, loader: "script" },
+	{ test: /p2\.js/, loader: "expose?p2" },
+];
+
+// define entries and entries output
+let entries = {
+    invaders: "./src/invaders.js",
+};
+let entries_output = {
+    path: "./dist",
+    filename: "[name].bundle.js"
+};
+
+module.exports = {
+	resolve: {
+        alias: {
+            "pixi": pixi,
+            "phaser": phaser,
+			"p2": p2,
+        }
+    },
+    entry: entries,
+    output: entries_output,
+    module: {
+        loaders: loaders,
+    },
+};
+
